b7c4be596ff68cb3a3d21239d12fb2b875331030
Shohei Aoki <shoaok@gmail.com>
2024-01-13 19:04:25 +0300

    create test code

--- app/test.py
+++ app/test.py
@@ -1,21 +1,21 @@
-# License: BSD
-# Author: Sasank Chilamkurthy
-
-from __future__ import print_function, division
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
+from torchvision import models, datasets, transforms
 import matplotlib.pyplot as plt
-import time
-import os
-import copy
+import os 
+import numpy as np
+from tqdm import tqdm
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-plt.ion()   # interactive mode
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 3)
+model_ft = model_ft.to(device)
+
+model_path = '../model/model_ft_gpu.pth'
+#Load model trained on GPU on CPU
+model_ft.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+
 
 data_transforms = {
     'train': transforms.Compose([
@@ -32,37 +32,34 @@ data_transforms = {
     ]),
 }
 
-data_dir = 'potato'
+data_dir = '../notebook/potato'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
                   for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                             shuffle=True, num_workers=4)
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=1,
+                                             shuffle=True, num_workers=0)
               for x in ['train', 'val']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 class_names = image_datasets['train'].classes
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+labels = []
+datas = []
+for i, (data, label) in enumerate(dataloaders['val']):
+    labels.append(label)
+    datas.append(data)
+len(datas)
 
-def imshow(inp, title=None):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
-
-
-# Get a batch of training data
-inputs, classes = next(iter(dataloaders['train']))
-
-# Make a grid from batch
-out = torchvision.utils.make_grid(inputs)
-
-imshow(out, title=[class_names[x] for x in classes])
+model_ft.eval()
+# x, y = image_datasets['val'][0][0], image_datasets['val'][0][1]
+passed = 0
+for i in tqdm(range(len(datas))):
+    x, y = datas[i], labels[i]
+    with torch.no_grad():
+        pred = model_ft(x)
+        predicted, actual = class_names[pred[0].argmax(0)], class_names[y]
+        # print(f'Predicted: "{predicted}", Actual: "{actual}"')
+        if predicted==actual:
+            passed += 1
+print(f"accuracy:{passed/len(datas)}")
 
 
